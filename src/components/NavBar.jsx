ebc857aa6bc2f5eb8040c3dc1046aa59b3d0db7d
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-04-26 17:32:39 +0300

    errors with the test are not passing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import "./NavBar.css"; // if you have styles
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
+
